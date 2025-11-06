db52286f728547ac3fd64f59a4a5ab4bf13ba1cb
Wonuola <wonuolalonge@gmail.com>
2025-11-06 16:50:37 +0100

    feat/bu-fix/6

--- app/auth/employer/login/page.tsx
+++ app/auth/employer/login/page.tsx
@@ -115,7 +115,7 @@ export default function LoginPage() {
               </div>
 
               <p className="text-center text-sm text-slate-500">
-                Don't have an account?{" "}
+                Don&apos;t have an account?{" "}
                 <Link
                   href="/auth/employer/signUp"
                   className="text-blue-600 hover:underline"
