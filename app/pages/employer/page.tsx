2dc3bc7eb3e4572cac25e180e977d634bd194699
Wonuola <wonuolalonge@gmail.com>
2025-11-15 19:30:12 +0100

    feat/bugfix

--- app/pages/employer/page.tsx
+++ app/pages/employer/page.tsx
@@ -11,7 +11,7 @@ import house from "@/app/assets/house.svg";
 import office from "@/app/assets/office.svg";
 import tinystar from "@/app/assets/tinyStar.svg";
 import solutions from "@/app/assets/solutions.svg";
-import Nav from "@/app/pages/components/employerNav";
+import Nav from "@/app/pages/components/employerNav"
 import hero from "@/app/assets/employersheroimg.svg";
 import dummyImage from "@/app/assets/dummyImage.png";
 import { Hospital } from "lucide-react";
