b6ebf67a74040d7e82a765e74a19bbe66139e4aa
Wonuola <wonuolalonge@gmail.com>
2025-11-18 15:49:38 +0100

    feat/bugfix

--- app/pages/components/employernav.tsx
+++ app/pages/components/employernav.tsx
@@ -1,8 +1,7 @@
 'use client';
-
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
 import logo from '@/app/assets/logo_white.svg';
 
 function Logo() {
